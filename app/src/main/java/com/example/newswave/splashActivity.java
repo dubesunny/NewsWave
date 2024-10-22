511a8e4a6a066e4551e06eac6f87cb151c99b8f3
Dharmesh <dubeydharmesh171@gmail.com>
2024-10-22 23:24:02 +0530

    "cimmitted"

---
+++ app/src/main/java/com/example/newswave/splashActivity.java
@@ -0,0 +1,27 @@
+package com.example.newswave;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splashActivity.this,MainActivity.class));
+
+            }
+        },3000);
+    }
+}
